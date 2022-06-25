9a92b2e63a4bd1c1f2928c6dfba3ec8b1cae78d4
Victoria David <victoria@thedavids.us>
2022-06-24 23:45:00 -0400

    shop feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -48,7 +48,7 @@ var fight = function(enemyName)
         //check enemy's health
         if (enemyHealth <= 0)
         {
-            window.alert(enemyName + " has died!");
+            window.alert(enemyName + " has died! You win 20 credits.");
             playerMoney = playerMoney + 20; //award player for winning
             break; //break loop bc enemy is dead
         } 
@@ -93,6 +93,14 @@ var startGame = function() //start a new game
             var pickedEnemyName = enemyNames[i]; //assign variable to the result of the <for>
             enemyHealth = 50;  //reset variable health state
             fight(pickedEnemyName);  //fight reset variable
+            if (playerHealth > 0 && i < enemyNames.length - 1) //access shop if i can still increase
+            {
+                var storeConfirm = window.confirm("Would you like to visit the store before starting the next round?")
+                if (storeConfirm)
+                {
+                    shop();
+                }
+            }
         }
         else
         {
@@ -119,6 +127,34 @@ var endGame = function() //end the game
         {
             window.alert("Thank you for playing Robot Gladiators! Come back soon!")
         }
+}; //end of endGame function
+
+var shop = function()
+{
+    console.log("Shop entered.")
+    //shop prompt function
+    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the shop? Choose one to enter below: REFILL, UPGRADE, LEAVE.");
+    //switch statement for shop functionality
+    switch (shopOptionPrompt)
+    {
+        case "refill":
+            window.alert("Refilling player's health by 20 for 7 credits.");
+            playerHealth = playerHealth + 20;
+            playerMoney = playerMoney - 7;
+            break;
+        case "upgrade":
+            window.alert("Upgrading player's attack by 6 for 7 credits.");
+            playerAttack = playerAttack + 6;
+            playerMoney = playerMoney -7;
+            break;
+        case "leave":
+            window.alert("Back to the fight!");
+            break;
+        default:
+            window.alert("Please type in a valid option: REFILL, UPGRADE, or LEAVE.");
+            shop();
+            break;
+    }
 };
 
 startGame(); //call startGame function
